1315b8d98fdcb890ae8e20531307cd457b5fc9a3
mayhong1 <maythegreat.minecraft@gmail.com>
2024-04-06 12:19:53 -0700

    updated home page UI

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+@app.route('/output', methods=['POST'])
+def output():
+    text_input = request.form['text_input']
+    return render_template('output.html')
 
 # this code runs the app in "development" mode which makes it easier when developing. 
 # comment out for final product
