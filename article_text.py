8cb12721501c3931dddd113e4561142f94ef622d
gr125 <gaurirenjith5@gmail.com>
2024-04-06 18:49:22 -0700

    use diff article

--- article_text.py
+++ article_text.py
@@ -11,7 +11,7 @@ subprocess.check_call([sys.executable, "-m", "pip", "install", 'goose3'])
 
 from goose3 import Goose
 
-article_url = "https://www.latimes.com/california/story/2024-04-04/electric-and-hybrid-cars-are-set-to-lose-carpool-access-what-you-need-to-know"
+article_url = "https://www.scientificamerican.com/article/how-ancient-humans-studied-and-predicted-solar-eclipses/"
 text, title = newspaper_text_extraction(article_url=article_url)
 print(title)
 print(text)
